93db8d000b6fbab62b46f1b4fa1a5935962da82a
Konstantin <kostya1703tsoy@gmail.com>
2024-02-16 17:01:33 +0300

    add time checker

--- portfoliio/src/components/Time/Time.jsx
+++ portfoliio/src/components/Time/Time.jsx
@@ -33,14 +33,54 @@ const Time = () => {
   const hours = duration.hours();
   const minutes = duration.minutes();
 
+  const monthsCheck = (num) => {
+    if ( num === 1 ) {
+      return "месяц"
+    } else if ( num > 1 &&  num < 5) {
+      return "месяца"
+    } else {
+      return "месяцев"
+    }
+  }
+
+  const daysCheck = (num) => {
+    if ( num === 1 || num === 21 || num === 31 ) {
+      return "день"
+    } else if ( ( num > 1 && num < 5 ) || ( num > 20 && num < 25) ) {
+      return "дня"
+    } else {
+      return "дней"
+    } 
+  }
+
+  const hoursCheck = (num) =>  {
+    if ( num === 1 || num === 21 ) {
+      return "час"
+    } else if ( ( num > 1 && num < 5 ) || ( num > 21 )) {
+      return "часа"
+    } else {
+      return "часов"
+    }
+  }
+
+  const minutesCheck = (num) => {
+    if ( num % 10 === 1 && num !== 11) {
+      return "минуту"
+    } else if ( num % 10 === 2 || num % 10 === 3 || num % 10 === 4) {
+      return "минуты"
+    } else {
+      return "минут"
+    }
+  }
+
   return (
     <section id="time" className={styles.section_time}>
       <div className={styles.block_time}>
         <p>В разработке интерфесов уже:</p>
         <p>
-          <span>{-months}</span> месяцев <span>{-days}</span> дней{" "}
+          <span>{-months}</span> {monthsCheck(-months)} <span>{-days}</span> {daysCheck(-days)}{" "}
           <br className={styles.spaceBetween} />
-          <span>{-hours}</span> часов <span>{-minutes}</span> минут
+          <span>{-hours}</span> {hoursCheck(-hours)} <span>{-minutes}</span> {minutesCheck(-minutes)}
         </p>
       </div>
     </section>
